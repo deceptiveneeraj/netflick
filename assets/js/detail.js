535461f2366a846f3ed1670d17c90b805770ed2a
Neeraj Solanki <neerajsolnk@gmail.com>
2026-01-17 11:43:09 +0530

    Upload series

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector('.detailImage').style.backgroundImage = `url('${showData.detailImg || 'assets/img/hero/default.png'}')`;
             document.querySelector('.match').innerHTML = `Ratings: ${info.rating || "8.0"}/10`;
             document.getElementById('detailYear').innerHTML = `Year: ${info.year || "2025"}`;
-            // document.querySelector('.type').innerHTML = `${top10[showName].type || "TV-MA"}`;
+            document.querySelector('.type').innerHTML = `${top10[showName].type || "TV-MA"}`;
             document.getElementById('detailGenre').innerHTML = `Genre: ${(info.genre).replaceAll(",", " | ") || "Action | Drama | Adventure | Fantasy"}`;
             document.getElementById('detailDesc').innerHTML = `Description: ${info.description || "No description available."}`;
             document.getElementById('cast').innerHTML = `Cast: ${info.actors || "Cast information not available."}`;
@@ -28,9 +28,46 @@ document.addEventListener('DOMContentLoaded', function() {
             // Display seasons if it's a TV show
             if (showData && showData.type === "TV Show" && showData.seasons) {
                 displaySeasons(showData.seasons, decodedShowName);
-            } else {
-                // Hide seasons section for movies
-                document.getElementById('seasonsSection').style.display = 'none';
+            }
+             else if(showData && showData.type === "Movie") {
+                const seasonsSection = document.getElementById('seasonsSection');
+                const seasonSelect = document.getElementById('seasonSelect');
+                
+                // Show seasons section
+                seasonsSection.style.display = 'block';
+                seasonSelect.style.display = 'none';
+
+                document.getElementById('episodes').innerHTML = "Movie";
+                document.getElementById('resolution').textContent = `Quality: ${showData.quality || 'HD'}`;
+
+                const movieThumbnail = showData.thumbnail || 'Image Not Found';
+                const movieTitle = showData.title || '';
+                const movieDescription = showData.description || '';
+                const movieLink = showData.link || "#";
+
+                let episodesHTML = '';
+                episodesHTML += `
+                <div class="episode-item">
+                    <a href="${movieLink}">
+                        <div class="episode-thumbnail" data-episode-number="${""}">
+                            <div class="episode-number">${""}</div>
+                            ${movieThumbnail ? `<img src="${movieThumbnail}" alt="${movieTitle}" onerror="this.style.display='none'">` : ''}
+                        </div>
+                        <div class="episode-info">
+                            <div class="episode-header">
+                                <h3 class="episode-title">${movieTitle}</h3>
+                                <span class="episode-duration">&DownArrowBar;</span>
+                            </div>
+                            <p class="episode-description">
+                                ${movieDescription}
+                            </p>
+                        </div>
+                    </a>
+                </div>
+                `;
+            
+            episodesList.innerHTML = episodesHTML;
+                    
             }
         });
     }
@@ -38,6 +75,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Function to display seasons and episodes
 function displaySeasons(seasons, showName) {
+    // document.getElementById('episodes').innerHTML = "Episodes";
     const seasonSelect = document.getElementById('seasonSelect');
     const episodesList = document.getElementById('episodesList');
     const seasonsSection = document.getElementById('seasonsSection');
